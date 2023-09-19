a24d33dadde82e008715826a7dd6a3f96e71cd5e
rybinali <rybinaaaaa.a@gmail.com>
2023-09-19 19:19:04 +0200

    add delete

--- src/main/java/rybina/config/mySpringMvcDispatcherInitializer.java
+++ src/main/java/rybina/config/mySpringMvcDispatcherInitializer.java
@@ -1,7 +1,11 @@
 package rybina.config;
 
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class mySpringMvcDispatcherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -17,5 +21,16 @@ public class mySpringMvcDispatcherInitializer extends AbstractAnnotationConfigDi
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
--- src/main/java/rybina/dao/PersonDAO.java
+++ src/main/java/rybina/dao/PersonDAO.java
@@ -30,4 +30,12 @@ public class PersonDAO {
         person.setId(PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatedPerson) {
+        this.show(id).setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
