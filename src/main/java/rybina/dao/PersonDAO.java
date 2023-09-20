2c0c95b740469fe6785f35b697fb1b45967fa579
rybinali <rybinaaaaa.a@gmail.com>
2023-09-20 15:42:50 +0200

    add db connection + add first request

--- src/main/java/rybina/dao/PersonDAO.java
+++ src/main/java/rybina/dao/PersonDAO.java
@@ -3,32 +3,64 @@ package rybina.dao;
 import org.springframework.stereotype.Component;
 import rybina.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String PASSWORD = "aqwsde322";
+    private static final String USER = "postgres";
 
-    {people = new ArrayList<Person>();
-    people.add(new Person(PEOPLE_COUNT++, "Mike", 29, "some email"));
-    people.add(new Person(PEOPLE_COUNT++, "Jack", 29, "some email"));
-    people.add(new Person(PEOPLE_COUNT++, "Nick", 29, "some email"));
-    people.add(new Person(PEOPLE_COUNT++, "Bob", 29, "some email"));
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM PERSON";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setAge(resultSet.getInt("age"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person){
         person.setId(PEOPLE_COUNT);
-        people.add(person);
+//        people.add(person);
     }
 
     public void update(int id, Person updatedPerson) {
@@ -39,6 +71,5 @@ public class PersonDAO {
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
     }
 }
