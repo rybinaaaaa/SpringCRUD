df93d955b6ec52119346e421a4f18900edfd6bff
rybinali <rybinaaaaa.a@gmail.com>
2023-09-23 17:37:00 +0200

    fix model

--- src/main/java/rybina/models/Book.java
+++ src/main/java/rybina/models/Book.java
@@ -6,15 +6,36 @@ import javax.validation.constraints.NotNull;
 
 public class Book {
 
+    @NotEmpty
+    private int id;
+
     @NotEmpty(message = "Name can not be empty")
     private String name;
 
-    @NotNull(message = "Book should have author")
-    private Person author;
+    @NotEmpty(message = "Book should have author")
+    private String author;
 
     @NotEmpty(message = "Year can not be empty")
     @Min(value = 1900, message = "Incorrect year")
-    private int release_year;
+    private int year;
+
+    private int person_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
 
     public String getName() {
         return name;
@@ -24,25 +45,37 @@ public class Book {
         this.name = name;
     }
 
-    public Person getAuthor() {
+    public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(Person author) {
+    public void setAuthor(String author) {
         this.author = author;
     }
 
-    public int getRelease_year() {
-        return release_year;
+    public int getYear() {
+        return year;
     }
 
-    public void setRelease_year(int release_year) {
-        this.release_year = release_year;
+    public void setYear(int year) {
+        this.year = year;
     }
 
-    public Book(String name, Person author, int release_year) {
+    public Book() {
+    }
+
+    public Book(int id, String name, String author, int year) {
+        this.id = id;
         this.name = name;
         this.author = author;
-        this.release_year = release_year;
+        this.year = year;
+    }
+
+    public Book(int id, String name, String author, int year, int person_id) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.person_id = person_id;
     }
 }
